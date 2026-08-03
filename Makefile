edd52a7180de3c506e4fe0c8b5f0eab8b6bd0ceb
sbcarnes <8630059+sbcarnes@users.noreply.github.com>
2026-08-03 14:48:20 -0600

    Initial empty window

---
+++ Makefile
@@ -0,0 +1,38 @@
+CC = gcc
+
+# Libraries to include
+LIBS = -lgdi32
+
+# Prevent terminal window also opening
+CFLAGS = -Wl,-subsystem,windows -mwindows -Wall -Wextra -Wpedantic
+
+# Main source file
+SRC = src/deckMain.c
+
+BUILD_DIR = build
+BIN_DIR = bin
+
+WINDRES = windres
+
+OBJ = $(SRC:src/%.c=$(BUILD_DIR)/%.o)
+
+# Output executable
+BIN = $(BIN_DIR)/RecallDeck.exe
+
+# Default target
+all: $(BIN)
+
+# Link object files into the exe
+$(BIN): $(OBJ)
+	@if not exist $(BIN_DIR) mkdir $(BIN_DIR)
+	$(CC) $(OBJ) -o $@ $(CFLAGS)
+
+# Compile .c file into .o files
+$(BUILD_DIR)/%.o: src/%.c
+	@if not exist $(BUILD_DIR) mkdir $(BUILD_DIR)
+	$(CC) -c $< -o $@ $(CFLAGS)
+
+# Clean up build artifacts
+clean:
+	-if exist $(BUILD_DIR) rmdir /s /q $(BUILD_DIR)
+	-if exist $(BIN_DIR) rmdir /s /q $(BIN_DIR)
---
+++ src/deckMain.c
@@ -0,0 +1,87 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const char g_szClassName[] = "myWindowClass";
+
+HBRUSH g_hbrBackground;
+COLORREF g_rgbBackground = RGB(255, 255, 255);
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    static RECT rcClient;    // client-area rectangle
+    
+    
+	switch(msg)
+	{
+		case WM_CREATE:
+            GetClientRect(hwnd, &rcClient);
+            
+        break;
+		case WM_CLOSE:
+			DestroyWindow(hwnd);
+		break;
+		case WM_DESTROY:
+            PostQuitMessage(0);
+		break;
+		default:
+			return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+    (void)hPrevInstance;
+    (void)lpCmdLine;
+    
+	WNDCLASSEX wc;
+	HWND hwnd;
+	MSG Msg;
+
+	wc.cbSize		 = sizeof(WNDCLASSEX);
+	wc.style		 = 0;
+	wc.lpfnWndProc	 = WndProc;
+	wc.cbClsExtra	 = 0;
+	wc.cbWndExtra	 = 0;
+	wc.hInstance	 = hInstance;
+	wc.hIcon		 = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor		 = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.lpszMenuName  = NULL;
+	wc.lpszClassName = g_szClassName;
+	wc.hIconSm		 = LoadIcon(NULL, IDI_APPLICATION);
+
+	if(!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Window Registration Failed!", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	hwnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		g_szClassName,
+		"Recall Deck",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 800, 500,
+		NULL, NULL, hInstance, NULL);
+
+	if(hwnd == NULL)
+	{
+		MessageBox(NULL, "Window Creation Failed!", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while(GetMessage(&Msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+	return Msg.wParam;
+}
